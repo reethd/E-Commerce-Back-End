6ae05d55de196b1cbb36c4cc1d2e5d8a92366427
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-01-31 11:54:33 -0500

    added starter code

---
+++ models/Tag.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
---
+++ test.js
@@ -0,0 +1,12 @@
+const connection = require("./config/connection");
+
+async function test() {
+  try {
+    await connection.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+test();
